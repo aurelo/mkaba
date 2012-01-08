08debe0dab90b26402b70cbf627533421d561b94
Zlatko Gudasic MAC <zlatko.gudasic@gmail.com>
2012-01-08 23:12:16 +0100

    Added mobile mime type and preparation for mobile layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :prepare_for_mobile
+
+  private
+
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+  helper_method :mobile_device?
+
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    if mobile_device?
+       request.format = :mobile 
+    else
+       #temporary for testing request format for desktop browsers also mobile
+       # TODO redirect to another info view to inform user that this is mobile optimized site and that for 
+       # desktop browser he/she should use desktop version of intenet banking
+       request.format = :mobile 
+       #redirect_to 'http://www.kaba.hr'  
+    end   
+  end
 end
