a9e3fbb7c70d298fda3e6494562d12cf843e2c25
Zlatko Gudasic MAC <zlatko.gudasic@gmail.com>
2012-01-17 07:44:06 +0100

    Financial institution view and mobile context

--- spec/requests/financial_institutions_spec.rb
+++ spec/requests/financial_institutions_spec.rb
@@ -9,10 +9,39 @@ describe "FinancialInstitutions" do
       response.status.should be(200)
     end
     
-    #it "should not equal bank index path" do
-    #  visit root_path
-     # current_path.should_not eq(bank_index_path)
-     ## save_and_open_page
-    #end
+    it "should have appropriate title" do
+      visit root_path
+      # page.should have_selector :title, :text => "mKaba"
+      page.should have_content "mKaba"
+    end
+    
+    context "links" do
+      it "should have Novosti link" do
+        visit root_path
+        page.should have_content "Novosti"
+        # save_and_open_page
+      end
+
+      it "should have mKaba link" do
+        visit root_path
+        page.should have_content "Demo mKaba"
+        # save_and_open_page
+      end
+      
+      it "should have Bankomati link" do
+        visit root_path
+        page.should have_content "Bankomati"
+        # save_and_open_page
+      end      
+
+      it "should have Poslovnice link" do
+        visit root_path
+        page.should have_content "Poslovnice"
+        # save_and_open_page
+      end
+
+      
+    end
+    
    end
 end
